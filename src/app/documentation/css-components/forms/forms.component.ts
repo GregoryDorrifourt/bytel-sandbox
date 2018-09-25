34cd7b8dcc1a782bd6484b9292a0c465d8c3debb
AGRAND <aurore@MacBook-Pro-de-Administrateur.local>
2018-09-25 16:19:17 +0200

    radio

--- src/app/documentation/css-components/forms/forms.component.ts
+++ src/app/documentation/css-components/forms/forms.component.ts
@@ -9,6 +9,7 @@ import { FormControl } from '@angular/forms';
 export class FormsComponent implements OnInit {
 
     public sampleCB: FormControl;
+    public sampleRadio: FormControl;
 
     constructor() {
 
@@ -16,6 +17,7 @@ export class FormsComponent implements OnInit {
 
     ngOnInit() {
         this.sampleCB = new FormControl('');
+        this.sampleRadio = new FormControl('');
     }
 
 }
