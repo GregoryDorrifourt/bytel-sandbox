cdd07d91ffcede53faa4a63e785b19be71737f35
AGRAND <aurore@MacBook-Pro-de-Administrateur.local>
2018-09-27 16:01:08 +0200

    copier code

--- src/app/documentation/css-components/buttons/buttons.component.ts
+++ src/app/documentation/css-components/buttons/buttons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CopyToClipboardService } from '../../../base/services/copy-to-clipboard.service';
 
 @Component({
     selector: 'sb-buttons',
@@ -9,8 +10,31 @@ import { Component, OnInit } from '@angular/core';
 
 export class ButtonsComponent implements OnInit {
 
+    public copied: boolean = false;
+
+    constructor(
+        private ctc: CopyToClipboardService
+    ) { }
+
+    private sample1 = `<button type="button" class="btn btn-primary">Primary</button>`;
+    private sample2 = `<button type="button" class="btn btn-outline-primary">Primary</button>`;
+    private sample3 = `<button type="button" class="btn btn-primary btn-lg">Large button</button>`;
+    private sample4 = `<button type="button" class="btn btn-primary btn-sm">Small button</button>`;
+    private sample5 = `<button type="button" class="btn btn-primary btn-lg btn-block">Block button</button>`;
+    private sample6 = `<button type="button" class="btn btn-primary btn-lg" disabled>Disabled button</button>`;
+
+
     ngOnInit() {
 
+    }
+
+    public copyCode(code: string): void {
+        this.ctc.copy(code).then(() => {
+            this.copied = true;
+            setTimeout(() => {
+                this.copied = false;
+            }, 2000);
+        });
     }
 
 }
